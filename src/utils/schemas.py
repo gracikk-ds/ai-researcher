fe87577fd9f0e542ac791da21bb7c230b0bade40
Aleksandr Gordeev <asgordeev@MacBook-Air-Aleksandr.local>
2025-06-22 15:13:05 +0300

    Add utility functions for price calculation and schemas for arXiv papers.

---
+++ src/utils/schemas.py
@@ -0,0 +1,30 @@
+"""Schemas for the papers."""
+
+from typing import Optional
+
+from pydantic import BaseModel
+
+
+class Author(BaseModel):
+    """Pydantic model representing an arXiv author."""
+
+    name: str
+    h_index: Optional[int] = None
+    paper_count: Optional[int] = None
+    citation_count: Optional[int] = None
+
+
+class Paper(BaseModel):
+    """Pydantic model representing an arXiv paper."""
+
+    title: str
+    authors: list[Author]
+    max_authors_h_index: Optional[int] = None
+    max_authors_paper_count: Optional[int] = None
+    max_authors_citation_count: Optional[int] = None
+    summary: str
+    published: str
+    arxiv_url: str
+    pdf_url: str
+    categories: list[str]
+    citation_count: Optional[int] = None
